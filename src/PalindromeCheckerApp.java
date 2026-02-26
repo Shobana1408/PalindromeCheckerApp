0b812d9d5e3f1fe4ff24513667109be21abfb8ae
Shobanasri G <shobanasrisrmcse@gmail.com>
2026-02-26 09:42:22 +0530

    feature/UC5

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,22 +1,21 @@
 import java.util.Scanner;
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
     public static void main(String[] args){
        Scanner text = new Scanner(System.in);
        System.out.print("Input : ");
        String input = text.nextLine();
-       char[] chars = input.toCharArray();
-       int start = 0;
-       int end = chars.length-1;
+       Stack<Character> stack = new Stack<>();
+       for (char c : input.toCharArray()){
+           stack.push(c);
+       }
        boolean isPalindrome = true;
-       while(start!=end){
-           if (chars[start]==chars[end]){
-               isPalindrome = true;
-               start++;
-               end--;
-           }
-           else{
-               isPalindrome = false;
-               break;
+       for (char c : input.toCharArray()){
+           if (stack.pop()==c){
+               isPalindrome=true;
+           }else {
+               isPalindrome=false;
            }
        }
        System.out.print("Is Palindrome? : "+isPalindrome);
